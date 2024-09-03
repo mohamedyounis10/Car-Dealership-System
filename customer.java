aa64c43647cbedefa352a98ea33119812bfb8303
mohamed younis <161056381+mohamedyounis10@users.noreply.github.com>
2024-09-04 01:22:26 +0300

    Customer.java

--- customer.java
+++ customer.java
@@ -1,15 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.myproject;
+import java.util.Scanner;
 
-/**
- *
- * @author moham
- */
-public class customer extends person{
-    String address;
-    String job;
-    
+public class Customer extends Person {
+    private String address;
+    private String job;
+
+    public static Scanner input = new Scanner(System.in);
+
+    // Default Constructor
+    public Customer() {
+        super();
+        System.out.println("Please enter your address:");
+        this.address = input.nextLine();
+        System.out.println("Please enter your job:");
+        this.job = input.nextLine();
+    }
+
+    // Parameterized Constructor
+    public Customer(String firstName, String lastName, String email, int phoneNumber, String address, String job) {
+        super(firstName, lastName, email, phoneNumber);
+        this.address = address;
+        this.job = job;
+    }
+
+    // Method to print customer details
+    @Override
+    public void print() {
+        super.print();
+        System.out.println("Address: " + address + " | Job: " + job);
+    }
+
+    // Getter and Setter for Address
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    // Getter and Setter for Job
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
 }
