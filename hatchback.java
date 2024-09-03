084121baf68500632b2a00f660feaa50a72e8fe3
mohamed younis <161056381+mohamedyounis10@users.noreply.github.com>
2024-09-04 01:23:22 +0300

    Hatchback.java

--- hatchback.java
+++ hatchback.java
@@ -1,13 +1,48 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.myproject;
+public class Hatchback extends Car {
+    private boolean hasRearSpoiler;
+    private boolean hasFoldableSeats;
 
-/**
- *
- * @author moham
- */
-public class hatchback  extends car{
-    
+    // Constructor to input Hatchback details
+    public Hatchback() {
+        super();  // Call the constructor of the Car class to initialize common attributes
+        System.out.println("Does the Hatchback have a rear spoiler? (true/false):");
+        this.hasRearSpoiler = input.nextBoolean();
+        System.out.println("Does the Hatchback have foldable seats? (true/false):");
+        this.hasFoldableSeats = input.nextBoolean();
+        input.nextLine(); // Clear the buffer
+    }
+
+    // Parameterized Constructor
+    public Hatchback(String make, String model, int year, double price, String color, boolean hasRearSpoiler, boolean hasFoldableSeats) {
+        super(make, model, year, price, color);
+        this.hasRearSpoiler = hasRearSpoiler;
+        this.hasFoldableSeats = hasFoldableSeats;
+    }
+
+    // Getter and Setter for hasRearSpoiler
+    public boolean isHasRearSpoiler() {
+        return hasRearSpoiler;
+    }
+
+    public void setHasRearSpoiler(boolean hasRearSpoiler) {
+        this.hasRearSpoiler = hasRearSpoiler;
+    }
+
+    // Getter and Setter for hasFoldableSeats
+    public boolean isHasFoldableSeats() {
+        return hasFoldableSeats;
+    }
+
+    public void setHasFoldableSeats(boolean hasFoldableSeats) {
+        this.hasFoldableSeats = hasFoldableSeats;
+    }
+
+    // Overridden method to print all Hatchback details
+    @Override
+    public void printCarInfo() {
+        super.printCarInfo();  // Call the print method of the Car class to print common details
+        System.out.println("Type of car Hatchback ");
+        System.out.println("Rear Spoiler: " + (this.hasRearSpoiler ? "Yes" : "No"));
+        System.out.println("Foldable Seats: " + (this.hasFoldableSeats ? "Yes" : "No"));
+    }
 }
